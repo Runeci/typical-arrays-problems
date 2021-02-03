a92a7c6dfc67169cbf3b807a0bc2364e16f47023
Runeci <kostenokd@mail.com>
2021-02-03 20:34:04 +0300

    feat: make max min avg functions work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max =function max (array) {
+    if(!array) { return [] }
+    if (array.length > 0){
+        return Math.max.apply(null, array);
+    } else {
+        return 0;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.min = function min (array) {
+    if(!array) { return [] }
+    if (array.length > 0){
+        return Math.min.apply(null, array);
+    } else {
+        return 0;
+    }
+};
+
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if(!array) { return [] }
+    if (array.length > 0){
+        let sum = array.reduce((sum, current) => sum + current, 0);
+        return (sum / array.length)
+    } else {
+        return 0;
+    }
+};
